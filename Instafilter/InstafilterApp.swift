0bb352ad32a08bb74394db4a3430b3d03cf0f5f4
mcantop <maciejcantop@icloud.com>
2022-10-02 23:17:06 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Maciej on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
